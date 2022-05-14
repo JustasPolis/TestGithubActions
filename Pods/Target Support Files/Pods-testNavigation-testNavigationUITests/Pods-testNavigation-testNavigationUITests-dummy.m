a8f92d4c81446fb55cdeaa124afbf2e194d7045a
Justin Polis <20723848+JustasPolis@users.noreply.github.com>
2022-05-14 14:35:46 +0300

    Delete Pods directory

--- Pods/Target Support Files/Pods-testNavigation-testNavigationUITests/Pods-testNavigation-testNavigationUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_testNavigation_testNavigationUITests : NSObject
-@end
-@implementation PodsDummy_Pods_testNavigation_testNavigationUITests
-@end
